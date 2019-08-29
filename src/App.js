030508ab1b95027503bed28e546adce3183f2a90
rezan21 <reza.naghshineh@gmail.com>
2019-08-29 18:43:51 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// open weather app api token
+const API_KEY = '9eb3640b6d4ec66c2ca3c4b0750c01c6';
+
+class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    // fetch data and save them in a const.
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/find?q=${city},${country}&units=metric&appid=${API_KEY}`
+    );
+    // convert the saved data to json files
+    const data = await api_call.json();
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.list[0].main.temp,
+        city: data.list[0].name,
+        country: data.list[0].sys.country,
+        humidity: data.list[0].main.humidity,
+        description: data.list[0].weather[0].description,
+        error: ''
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the city and country.'
+      });
+    }
+  };
+  render() {
+    return (
+      <div>
+        <div className='wrapper'>
+          <div className='main'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-xs-5 title-container'>
+                  <Titles />
+                </div>
+                <div className='col-xs-7 form-container'>
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Titles = () => {
+  return (
+    <div>
+      <h1 className='title-container__title'>Weather App</h1>
+      <h2 className='title-container__subtitle'>
+        Find out temperature, conditions and more...
+      </h2>
+      <h3>An app made with react.js library</h3>
+    </div>
+  );
+};
+
+export default Titles;
